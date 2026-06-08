aa754307c5c738fc3a1833b6d70b299b7c65601c
Natanael Arndt <arndtn@gmail.com>
2026-06-08 15:11:47 +0200

    further implement indexer job

--- examples/airflow/dags/job-index.py
+++ examples/airflow/dags/job-index.py
@@ -14,17 +14,10 @@ secret_env_secret_access_key = Secret(
 
 sparql_update_endpoint = "http://webarchive-fuseki:3030/ds/update"
 
-TARGET_BUCKET_NAME = "webarchive"
-
-ARAS_REST_BASE = "http://etc.dnb.de/aras/"
-ARAS_REPO = "warc"
-
-PROV_IRI = "https://webarchiv.dnb.de/workflow/recompress/v1"
-
 
 @dag(
     schedule=None,  # "@once"
-    description="A recompress dag",
+    description="An indexer dag",
     tags=["wacli"],
 )
 def job_index():
@@ -35,102 +28,46 @@ def job_index():
         env_vars={
             "AWS_ENDPOINT_URL_S3": "http://webarchive-versitygw:7070",
             "AWS_DEFAULT_REGION": "eu-central-1",
+            "OUTBACK_CDX_URL": "http://outbackcdx-service:8080/outbackcdx/",
+            "WARC_COLLECTION": "warcs",
         },
         do_xcom_push=True,
     )
     def index(job: dict):
-        from warcio.recompressor import StreamRecompressor
+        from warcio.indexer import Indexer
         from s3fs import S3FileSystem
+        import tempfile
+        import os
+        import requests
+
+        DEFAULT_CDX_FILEDS = "offset,warc-type,warc-target-uri"
+        cdx_url = os.environ.get("OUTBACK_CDX_URL")
+        collection = os.environ.get("WARC_COLLECTION")
+
+        cdx_endpoint = f"{cdx_url}{collection}"
 
         s3 = S3FileSystem()
 
-        try:
-            s3.mkdir(TARGET_BUCKET_NAME, create_parents=True)
-        except FileExistsError:
-            pass
-
         print(
-            f"I will now download the file {job['source_file']} (bucket: {job['source_bucket']}, filename: {job['source_filename']}), recompress it and upload it to the s3 bucket {TARGET_BUCKET_NAME}. ({job['job_iri']})."
+            f"I will now download the file {job['source_file']} (bucket: {job['source_bucket']}, filename: {job['source_filename']}), index it and push the cdx record to outbackcdx. ({job['job_iri']})."
         )
 
-        # Download the file according to the graphs file spec
-        # recompress it and upload it
+        path_in_s3fs = f"s3://{job['source_bucket']}/{job['source_filename']}"
 
-        #!/bin/sh
-        # id
-        # cd /data/warcs
-        # ls -la
-        # for warc_file in */*
-        # do
-        # echo ${warc_file}
-        # mkdir -p "/data/cdx/$(dirname "$warc_file")"
-        # done
-        # cdx-indexer --sort --recurse --output ../cdx/ .
 
-        # cd /data/cdx
-        # ls -la
-        # for cdx_file in */*
-        # do
-        # echo ${cdx_file}
-        # curl -X POST --data-binary @${cdx_file} http://outbackcdx-service:8080/outbackcdx/{{ .Values.pywbCollection }}
-        # done
+        # Create a temporary file using Python's tempfile module
+        # with tempfile.NamedTemporaryFile(mode='w', suffix='.cdx') as tmp_cdx_file:
+        with tempfile.TemporaryFile(mode="w+", suffix=".cdx") as tmp_cdx_file, s3.open(path_in_s3fs, "rb") as source_file:
+            Indexer(DEFAULT_CDX_FILEDS, [], None).process_one(source_file, output=tmp_cdx_file, filename=path_in_s3fs)
 
-        return job
+            tmp_cdx_file.seek(0)
+            cdx_content = tmp_cdx_file.read()
+            print(cdx_content)
+            response = requests.post(cdx_endpoint, data=cdx_content)
+            response.raise_for_status()
 
-    @task
-    def register_files(job: dict):
-        import requests
+            print(f"Successfully uploaded CDX data for {job['source_filename']}")
 
-        file_iris = {
-            "https://example.org/file/" + file_name: file_name
-            for file_name in job["files"]
-        }
-
-        file_update = (
-            """
-        PREFIX wa: <https://webarchiv.dnb.de/>
-        PREFIX wal: <https://d-nb.info/standards/elementset/wal#>
-        PREFIX prov: <http://www.w3.org/ns/prov#>
-
-        INSERT DATA {
-            GRAPH wa:data {
-        """
-            + "\n".join(
-                [
-                    f'<{file_iri}> a wal:File ; wal:filename "{file_name}"; wal:bucket "{TARGET_BUCKET_NAME}" .'
-                    for file_iri, file_name in file_iris.items()
-                ]
-            )
-            + """
-            }
-            GRAPH wa:prov {
-        """
-            + "\n".join(
-                [
-                    f"<{file_iri}> prov:wasGeneratedBy <{job['job_iri']}> ; prov:wasAttributedTo <{PROV_IRI}>; prov:wasDerivedFrom <{file_iri}> ."
-                    for file_iri, file_name in file_iris.items()
-                ]
-            )
-            + """
-            }
-        }
-        """
-        )
-
-        r = requests.post(
-            sparql_update_endpoint,
-            auth=("admin", "admin"),
-            headers={
-                "Accept": "application/sparql-results+json,*/*;q=0.9",
-                "Content-Type": "application/sparql-update",
-            },
-            data=file_update,
-        )
-
-        print(r)
-        print(r.text)
-
-        r.raise_for_status()
         return job
 
     triple_pattern = """
@@ -139,17 +76,15 @@ def job_index():
     """
 
     jobs_done(
-        job=index.expand(
+        index.expand(
             job=get_jobs(
-                "?source_file ?source_filename ?source_bucket",
-                "wal:RecompressJob",
+                ["source_file", "source_filename", "source_bucket"],
+                "wal:IndexJob",
                 {"wal:file": "?source_file"},
                 triple_pattern=triple_pattern,
             )
         )
-
     )
-    # job_done.expand(job=job_execution.expand(job=get_jobs("?idn", "wal:ArasPullJob", {"wal:idn": "?idn"})))
 
 
 job_index()
